2dd8eca2cbafbbdc47755a3ae72cb10289e97618
nilishalu <shalini.c.e.ganu@gmail.com>
2023-07-27 21:26:59 +0530

    add api endpoints for group chat

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -43,7 +43,96 @@ const chat = async (req, res) => {
 };
 
 const fetchChats = async (req, res) => {
-    
+    console.log(req.user)
+    try {
+        Chat.find({users: {$elemMatch: {$eq: req.user._id}}})
+        .populate("users", "-password")
+        .populate("admin", "-password")
+        .populate("latestMessage")
+        .sort({ updatedAt: -1 })
+        .then(async (data) => {
+           data = await User.populate(data, {
+            path: "latestMessage.sender",
+            select: "name picture email"
+           })
+
+           res.status(200).send(data);
+        })
+    } catch (error) {
+        res.status(400).send({message: "An error occurred"});
+    }
 };
 
-module.exports = { chat, fetchChats };
+const createGroup = async (req, res) => {
+    let { name, users } = req.body;
+    if (!name || !users) {
+        return res.status(400).send({ message: "Please fill all details" });
+    }
+
+    users = JSON.parse(users);
+
+    users.push(req.user);
+
+    try {
+        const group = await Chat.create({
+            chatName: name,
+            users: users,
+            isGroupChat: true,
+            admin: req.user
+        });
+
+        const fullChat = await Chat.findOne({ _id: group._id }).populate("users", "-password").populate("admin", "-password");
+        
+        res.status(200).json(fullChat);
+    } catch (error) {
+        res.status(400).json({message: "An error occurred"})
+    }
+};
+
+const renameGroup = async (req, res) => {
+    const { chatId, chatName } = req.body;
+
+    const newName = await Chat.findByIdAndUpdate(chatId, {
+        chatName
+    },
+    { new: true}).populate("users", "-password")
+    .populate("admin", "-password");
+
+    if (!newName) {
+        res.status(404);
+    } else {
+        res.json(newName);
+    }
+};
+
+const addMember = async (req, res) => {
+    const { chatId, userId } = req.body;
+
+    const added = await Chat.findByIdAndUpdate(chatId, {
+        $push: {users: userId},
+    }, {new: true}).populate("users", "-password")
+    .populate("admin", "-password");
+
+    if (!added) {
+        res.status(404);
+    } else {
+        res.json(added);
+    }
+};
+
+const removeMember = async (req, res) => {
+    const { chatId, userId } = req.body;
+
+    const removed = await Chat.findByIdAndUpdate(chatId, {
+        $pull: {users: userId},
+    }, {new: true}).populate("users", "-password")
+    .populate("admin", "-password");
+
+    if (!removed) {
+        res.status(404);
+    } else {
+        res.json(removed);
+    }
+};
+
+module.exports = { chat, fetchChats, createGroup, renameGroup, addMember, removeMember };
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const { author } = require("../authorization/authUser");
-const { chat, fetchChats } = require("../controllers/chatController");
+const { chat, fetchChats, createGroup, renameGroup, addMember, removeMember } = require("../controllers/chatController");
 
 const router = express.Router();
 
 router.route('/').post(author, chat);
 router.route('/').get(author, fetchChats);
+router.route('/group').post(author, createGroup);
+router.route('/rename').put(author, renameGroup);
+router.route('/group_remove').put(author, removeMember);
+router.route('/group_add').put(author, addMember);
 
 module.exports = router;
